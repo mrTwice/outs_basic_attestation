048f7b329ea67a379d99029efd95f6bb2a23696b
mrTwice <yampolskiy86@gmail.com>
2024-08-18 10:26:02 +0300

    remove unused methods

--- application/src/main/java/ru/otus/basic/yampolskiy/servlets/models/Route.java
+++ application/src/main/java/ru/otus/basic/yampolskiy/servlets/models/Route.java
@@ -1,10 +1,7 @@
 package ru.otus.basic.yampolskiy.servlets.models;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Route {
     private final Servlet servlet;
@@ -29,34 +26,4 @@ public class Route {
         return originalPath;
     }
 
-    public Parameter[] getParameters() {
-        return method.getParameters();
-    }
-
-    public Class<?>[] getParameterTypes() {
-        return method.getParameterTypes();
-    }
-
-    public Annotation[][] getParameterAnnotations() {
-        return method.getParameterAnnotations();
-    }
-
-    public Map<String, String> extractPathVariables(String requestPath) {
-        String[] originalParts = originalPath.split("/");
-        String[] requestParts = requestPath.split("/");
-        Map<String, String> variables = new HashMap<>();
-
-        if (originalParts.length != requestParts.length) {
-            throw new IllegalArgumentException("Путь запроса не совпадает с шаблоном по количеству сегментов");
-        }
-
-        for (int i = 0; i < originalParts.length; i++) {
-            if (originalParts[i].startsWith("{") && originalParts[i].endsWith("}")) {
-                String varName = originalParts[i].substring(1, originalParts[i].length() - 1);
-                variables.put(varName, requestParts[i]);
-            }
-        }
-
-        return variables;
-    }
 }
